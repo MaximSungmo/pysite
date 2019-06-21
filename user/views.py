9548dcddb17397f8d099ed88c2c419159be9a5ff
MaximSungmo <ksm5318@naver.com>
2019-06-21 11:07:24 +0900

    session 처리

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.contrib import auth
 from django.contrib.auth import authenticate
+from django.forms import model_to_dict
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -25,17 +26,53 @@ def joinsuccess(request):
     return render(request, 'user/joinsuccess.html')
 
 
+def loginform(request):
+    return render(request, 'user/loginform.html')
+
+
 def login(request):
-    if request.method == "GET":
-        return render(request, 'user/loginform.html')
-    elif request.method == "POST":
-        email = request.POST['email']
-        password = request.POST['password']
-        user = User.objects.get(email=email, password=password)
-        if user:
-            # login(request, user=user)
-            return HttpResponseRedirect('/')
-        else:
-            return render(request, 'user/loginform.html')
+    results = User.objects.filter(email=request.POST['email']).filter(password=request.POST['password'])
+    # 로그인 실패
+    if len(results) == 0:
+        return HttpResponseRedirect('/user/loginform?result=fail')
+
+    # 로그인 처리
+    # 현재는 객체로 만들어진 상태
+    authuser = results[0]
+    # 객체를 dictionary 형태로 변경하여 저장
+    request.session['authuser'] = model_to_dict(authuser)
+
+    return HttpResponseRedirect('/')
+    # user = User.objects.get(email=email, password=password)
+    # if user:
+    #     # login(request, user=user)
+    #     return HttpResponseRedirect('/')
+    # else:
+    #     return render(request, 'user/loginform.html')
 
 
+def logout(request):
+    # 세션 제거하기
+    del request.session['authuser']
+    return HttpResponseRedirect('/')
+
+
+def updateform(request):
+    user = User.objects.get(id=request.session['authuser']['id'])
+    data = {
+        'user': user
+    }
+    return render(request, 'user/updateform.html', data)
+
+
+def update(request):
+    user = User.objects.get(id=request.session['authuser']['id'])
+    user.name = request.POST['name']
+    user.gender =  request.POST['gender']
+    if request.POST['password'] is not '':
+        user.password = request.POST['password']
+    else:
+        return HttpResponseRedirect('/user/updateform?result=fail')
+    user.save()
+    request.session['authuser'] = model_to_dict(user)
+    return HttpResponseRedirect('/user/updateform?result=success')
